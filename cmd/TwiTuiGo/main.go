4f3dddaba4e9795a355f36e3216684bbd0d16a54
DmitriyBrch <organizm999@gmail.com>
2023-01-18 19:39:20 +0200

    working streamrun with ./streamlink in directory

--- cmd/TwiTuiGo/main.go
+++ cmd/TwiTuiGo/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/PeepoFrog/TwiTuiGo/internal/controller"
 	"github.com/PeepoFrog/TwiTuiGo/internal/model"
 	// "github.com/PeepoFrog/TwiTuiGo/internal/tui/tview"
 	"github.com/PeepoFrog/TwiTuiGo/internal/tui/bubbletea"
+	// "github.com/PeepoFrog/TwiTuiGo/internal/apiTesting"
 	"github.com/PeepoFrog/TwiTuiGo/internal/tui/tuiTesting"
 	"log"
 	"os"
@@ -21,53 +21,14 @@ func main() {
 		fmt.Println(err)
 		log.Fatalf("Error loading .env file")
 	}
+	//
 	authToTwitch.ClientID = os.Getenv("ClientID")
 	authToTwitch.ClientSecret = os.Getenv("TwitchToken")
 	authToTwitch.AccessToken = os.Getenv("AccessToken")
-	// usingtest()
+	//
 	bubbleteaTUI.AuthToTwitch = authToTwitch
-	bubbleteaTUI.Run()
-
 	tuiTesting.AuthToTwitch = authToTwitch
-	// bubbleteaUserInterface.Run()
-	// tuiTesting.RunV2()
+	// bubbleteaTUI.Run()
+	tuiTesting.Run()
 
-}
-
-func usingtest() {
-	gamesCursor := ""
-	streamsCursor := ""
-	games, _ := controller.GetGames(&authToTwitch, gamesCursor)
-	gamesCursor = games.Pagination.Cursor
-	printGamesResponse(&games)
-	games, _ = controller.GetGames(&authToTwitch, gamesCursor)
-	gamesCursor = games.Pagination.Cursor
-	printGamesResponse(&games)
-	streams, _ := controller.GetStreamsFromSelectedGame(&authToTwitch, streamsCursor, "29595")
-	streamsCursor = streams.Pagination.Cursor
-	printStreamsResponse(&streams)
-	streams, _ = controller.GetStreamsFromSelectedGame(&authToTwitch, streamsCursor, "29595")
-	streamsCursor = streams.Pagination.Cursor
-	printStreamsResponse(&streams)
-}
-func printGamesResponse(games *model.Games) {
-	for a, b := range games.Data {
-		fmt.Println(
-			a,
-			"ID:", b.ID,
-			"Name:", b.Name,
-			"IGBDid:", b.IGBDid)
-	}
-	fmt.Println(games.Pagination.Cursor)
-}
-func printStreamsResponse(games *model.Streamers) {
-	for a, b := range games.Data {
-		fmt.Println(
-			a,
-			"gameID:", b.GameID,
-			"Game:", b.GameName,
-			"UserName:", b.UserName,
-			"Vievers", b.ViewerCount)
-	}
-	fmt.Println(games.Pagination.Cursor)
 }
